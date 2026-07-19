566194b0c436fdda8163d46635e8f5bb554f53f9
pduongpdu99 <pduongpdu99@gmail.com>
2026-07-19 14:14:26 +0700

    update categories dto

--- src/api/categories/dto/create-category.dto.ts
+++ src/api/categories/dto/create-category.dto.ts
@@ -1,1 +1,20 @@
-export class CreateCategoryDto {}
+import { IsString } from 'class-validator'
+
+export class CreateCategoryDto {
+    created_at?: string
+    updated_at?: string
+    created_by?: string
+    updated_by?: string
+
+    @IsString()
+    slug: string = ""
+
+    @IsString()
+    title: string = ""
+
+    @IsString()
+    description: string = ""
+
+    @IsString()
+    icon: string = ""
+}
--- src/api/categories/dto/update-category.dto.ts
+++ src/api/categories/dto/update-category.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
+import { IsInt } from 'class-validator';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+    @IsInt()
+    id: number = 0
+}
