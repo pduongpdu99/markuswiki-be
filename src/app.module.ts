7cf4b76a7efdaf7668db6229bcdc09777f4132ad
pduongpdu99 <pduongpdu99@gmail.com>
2026-07-20 11:08:41 +0700

    ping pong api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoriesModule } from './api/categories/categories.module';
 import { TagsModule } from './api/tags/tags.module';
 import { ArticleModule } from './api/article/article.module';
+import { PrismaModule } from '@providers/prisma.module';
 
 @Module({
-  imports: [CategoriesModule, TagsModule, ArticleModule],
+  imports: [CategoriesModule, TagsModule, ArticleModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
