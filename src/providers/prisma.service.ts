463cd8c38e28e040c7b4be7d769eb79e3dcbb984
pduongpdu99 <pduongpdu99@gmail.com>
2026-07-20 11:09:08 +0700

    add globalized prisma module

--- src/providers/prisma.service.ts
+++ src/providers/prisma.service.ts
@@ -1,18 +1,35 @@
-
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger(PrismaService.name);
+
     constructor() {
         const adapter = new PrismaMariaDb({
-            host: process.env.DATABASE_URL,
-            port: 3306,
-            user: process.env.DATABASE_URL,
-            password: process.env.DATABASE_URL,
-            connectionLimit: 5
+            host: process.env.DATABASE_HOST,
+            port: Number(process.env.DATABASE_PORT),
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            connectionLimit: 5,
+            logger: {
+                error: (err) => console.error('Driver Error:', err),
+                warning: (warn) => console.warn('Driver Warn:', warn),
+            }
+
         });
-        super({ adapter });
+        super({ adapter, log: ['query', 'info', 'warn', 'error'] });
+    }
+
+    async onModuleInit() {
+        try {
+            this.logger.log("Prisma is connecting to MariaDB...");
+            await this.$connect();
+            console.log("Prisma service initialization with MariaDB completed.");
+        } catch (error) {
+            this.logger.error("Database connection failed:", error);
+        }
     }
 }
