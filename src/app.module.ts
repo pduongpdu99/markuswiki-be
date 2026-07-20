c8341ad92a7b40ec84eb87b8c3b2c51aa8662a03
pduongpdu99 <pduongpdu99@gmail.com>
2026-07-20 14:21:12 +0700

    update suffix structural response

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { CategoriesModule } from './api/categories/categories.module';
 import { TagsModule } from './api/tags/tags.module';
 import { ArticleModule } from './api/article/article.module';
 import { PrismaModule } from '@providers/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SuffixResponse } from './interceptors/suff-response';
 
 @Module({
   imports: [CategoriesModule, TagsModule, ArticleModule, PrismaModule],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: SuffixResponse
+    }
+  ]
 })
 export class AppModule { }
--- src/interceptors/suff-response.ts
+++ src/interceptors/suff-response.ts
@@ -2,10 +2,13 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nes
 import { catchError, map, throwError, timeout } from "rxjs";
 
 @Injectable()
-export class TimeoutValidation implements NestInterceptor {
+export class SuffixResponse implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler) {
         return next.handle().pipe(
-            timeout(5000),
+            map(val => ({
+                message: "success",
+                data: val
+            })),
             catchError((error) => {
                 return throwError(() => new Error(error));
             })
